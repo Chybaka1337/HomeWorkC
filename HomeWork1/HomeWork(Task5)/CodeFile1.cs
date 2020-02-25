f89e78b87dc7ed67019e958f94f906c515792cf3
Zommer Andrey <zommer.03@gmail.com>
2020-02-25 12:32:57 +0300

    Домашняя работа - 1

---
+++ HomeWork1/HomeWork(Task5)/CodeFile1.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Вывод в центр окна консоли
+ */using System;
+
+namespace HomeWork
+{
+    class task5
+    {
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+
+        static void Main()
+        {
+            Console.SetWindowSize(70, 30);
+            Console.SetBufferSize(70, 30);
+            Console.SetCursorPosition(25, 13);
+
+            Console.WriteLine("Зоммер Андрей, город Тула");
+
+            Pause();
+        }
+    }
+}
